597014105cbbf6c669529a5311b0daba39de7fdb
FranciscaA <fr.arancibiac@duocuc.cl>
2026-06-16 17:28:17 -0400

    Implementacion de react y tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-900">
+      <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-fuchsia-500">
+        ¡Tailwind + React funcionando, Fran!
+      </h1>
+    </div>
+  )
+}
+
+export default App
